b484efba9af28930500afdd57e047be8b6d446dc
Julio Espinoza <espinozajc@gmail.com>
2026-07-29 11:03:05 -0400

    fix: map credito inversiones to bci

--- src/connectors/destinations/rex_empleados.js
+++ src/connectors/destinations/rex_empleados.js
@@ -179,6 +179,7 @@ const HEALTH_NAME_ALIASES = {
 };
 
 const BANK_NAME_ALIASES = {
+  'credito inversiones': 'Banco BCI',
   'de chile': 'Banco de CHILE',
   chile: 'Banco de CHILE',
   'banco chile': 'Banco de CHILE',
